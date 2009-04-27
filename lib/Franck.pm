950baadc43500f428a54838427b5a9a1e8eaca07
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-04-27 10:59:35 +0900

    initial commit

---
+++ lib/Franck.pm
@@ -0,0 +1,33 @@
+package Franck;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Franck -
+
+=head1 SYNOPSIS
+
+  use Franck;
+
+=head1 DESCRIPTION
+
+Franck is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom  slkjfd gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
